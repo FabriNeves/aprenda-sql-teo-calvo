2070481801d8dee90555ad49619f620302850adc
Fabricio Dev <eng.fbneves@gmail.com>
2025-09-11 23:36:32 -0300

    Exercício 07 - Utilizando o comando WHERE com LIKE - Listar todos os produtos que contenham a palavra "chapéu" em sua descrição

---
+++ Exercicios/07_where.sql
@@ -0,0 +1,4 @@
+-- Exercício 07 - Utilizando o comando WHERE com LIKE
+-- Listar todos os produtos que contenham a palavra "chapéu" em sua descrição
+SELECT * FROM produtos  WHERE DescProduto LIKE '%hapéu%';
+ 
