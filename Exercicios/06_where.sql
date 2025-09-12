627601f82be4b8264d75b074cd0b1b5ce86bed97
Fabricio Dev <eng.fbneves@gmail.com>
2025-09-11 22:46:53 -0300

    Exercicio 06: Utilizando o operador LIKE, escreva uma consulta SQL para encontrar todos os produtos cujo nome termina com a palavra 'over'.

---
+++ Exercicios/06_where.sql
@@ -0,0 +1,2 @@
+-- Exercicio 06: Utilizando o operador LIKE, escreva uma consulta SQL para encontrar todos os produtos cujo nome termina com a palavra 'over'.
+SELECT * FROM produtos WHERE  DescProduto LIKE '%over';
