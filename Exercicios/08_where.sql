6c80baa98100dd3d4bae73e575066c50538d17f5
Fabricio Dev <eng.fbneves@gmail.com>
2025-09-11 23:39:42 -0300

    -- Exercício 08 - Utilizando o comando WHERE com LIKE -- Listar todos os produtos que contenham a palavra "Resgatar Ponei"

---
+++ Exercicios/08_where.sql
@@ -0,0 +1,4 @@
+-- Exercício 08 - Utilizando o comando WHERE com LIKE
+-- Listar todos os produtos que contenham a palavra "Resgatar Ponei"
+
+SELECT * FROM produtos WHERE DescProduto LIKE '%Resgatar Ponei%';
