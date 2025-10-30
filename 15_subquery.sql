b443d6a8d0c987b2c97d850c00aa96ce81b17880
Fabricio Dev <eng.fbneves@gmail.com>
2025-10-29 22:48:27 -0300

    Adiciona consulta SQL para listar transações de clientes em 2025-08-29 com base em transações de 2025-08-25

---
+++ 15_subquery.sql
@@ -0,0 +1,7 @@
+
+SELECT * FROM transacoes as t1 
+WHERE t1.IdCliente IN 
+(SELECT DISTINCT IdCliente
+FROM transacoes
+WHERE substr(DtCriacao,1,10) = '2025-08-25')
+AND substr(t1.DtCriacao,1,10) = '2025-08-29';
